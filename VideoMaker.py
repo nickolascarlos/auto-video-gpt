85b2c22a2a3e6cdc516c45f2ea71360866bbe9cf
Nickolas Carlos <nickolascarlos@users.noreply.github.com>
2023-10-02 09:54:35 -0300

    refactor: Replaces VidGear save with MoviePy save

--- VideoMaker.py
+++ VideoMaker.py
@@ -1,7 +1,4 @@
 from moviepy.editor import CompositeVideoClip
-from vidgear.gears import WriteGear
-import numpy as np
-import cv2
 
 class VideoMaker:
 
@@ -39,17 +36,4 @@ class VideoMaker:
         return CompositeVideoClip([*self.background(self), *self.content(self), *self.watermark(self)])
     
     def save(self, path):
-        # Define video parameters
-        # output_params = {
-        #     "filename": "random_square_video.mp4",  # Output filename
-        #     "compression": None,
-        #     "fps": 24,  # Frames per second
-        #     "frame_shape": (self.width, self.height),  # Frame dimensions (width, height)
-        # }
-
-        baked_video = self.make()
-
-        # Initialize the VidGear writer
-        with WriteGear(output="random_square_video.mp4", compression_mode=True, logging=True, **{'-vcodec': 'libx264', '-b:v': '2000k'}) as writer:
-            for frame in baked_video.iter_frames(fps=self.fps, dtype='uint8'):
-                writer.write(cv2.cvtColor(frame, cv2.COLOR_RGBA2BGRA))
+        self.make().write_videofile(path, fps=self.fps, threads=4)
